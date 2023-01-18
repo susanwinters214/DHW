598fd1e3940e6301ae27bced18d00adff41269bb
Susan Winters <susanwinters214@gmail.com>
2023-01-18 17:59:28 -0600

    corrected Script tag for GA

--- pages/_document.js
+++ pages/_document.js
@@ -15,14 +15,20 @@ class MyDocument extends Document {
             defer
           ></script>
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8N43XDXPPJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+     
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-8N43XDXPPJ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-  gtag('config', 'G-8N43XDXPPJ');
-</script>
+          gtag('config', 'G-8N43XDXPPJ');
+        `}
+          </Script>
         </Head>
         <Script src="js/jquery.min.js" strategy="beforeInteractive"></Script>
 
