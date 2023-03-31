f1b89377f0c26d4ec143edd51b756ce8ff300a20
Susan Winters <susanwinters214@gmail.com>
2023-03-30 19:32:51 -0500

    added where to find us

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -7,10 +7,7 @@ function SitemapPage(props) {
     <Fragment>
       <Head>
         <title>Dallas Home Watch Sitemap</title>
-        <link
-          rel="canonical"
-          href="https://www.dallashomewatch.com/sitemap"
-        />
+        <link rel="canonical" href="https://www.dallashomewatch.com/sitemap" />
         <meta
           name="description"
           content="Sitemap for Dallas Home Watch website. If you have trouble finding anything our website, reach out to our website developer."
@@ -140,6 +137,11 @@ function SitemapPage(props) {
                   </h4>
                   <h4>
                     <Link href="/terms-of-service">Terms of Service</Link>
+                  </h4>
+                  <h4>
+                    <Link href="/where-to-find-dallas-home-watch">
+                      Where To Find Us
+                    </Link>
                   </h4>
                 </section>
               </div>
---
+++ pages/where-to-find-dallas-home-watch.js
@@ -0,0 +1,259 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+function WhereToFindPage() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Where To Find Dallas Home Watch</title>
+        <link
+          rel="canonical"
+          href="https://www.dallashomewatch.com/where-to-find-dallas-home-watch"
+        />
+        <meta
+          name="description"
+          content="Where to find Dallas Home Watch on the web. Dallas Home Watch is listed on many online directories but sometimes 'Home Watch' is
+          not listed as a category."
+        />
+        <meta property="fb:app_id" content="8620778781329629" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Dallas Home Watch" />
+
+        <meta
+          property="og:url"
+          content="https://www.dallashomewatch.com/where-to-find-dallas-home-watch"
+        />
+        <meta property="og:title" content="Where To Find Dallas Home Watch" />
+        <meta
+          property="og:description"
+          content="Where to find Dallas Home Watch on the web. Dallas Home Watch is listed on many online directories but sometimes 'Home Watch' is
+          not listed as a category."
+        />
+        <meta
+          property="og:image"
+          content="https://www.dallashomewatch.com/images/og/dallas-skyline-service-areas.jpg"
+        />
+        <meta property="og:image:width" content="1200px" />
+        <meta property="og:image:height" content="630px" />
+        <meta
+          property="og:image:alt"
+          content="city of Dallas neighborhood in downtown"
+        />
+      </Head>
+      {/* insert top banner with Page Title & Breadcrumb*/}
+      <section className="page-title pageback-about">
+        <div className="auto-container">
+          <h1>Where To Find Dallas Home Watch</h1>
+          <ul className="page-breadcrumb">
+            <li>
+              <Link href="/">home</Link>
+            </li>
+            <li>Where To Find Home Watch</li>
+          </ul>
+        </div>
+      </section>
+      <section className="case-section">
+        <div className="auto-container">
+          <div className="inner-container">
+            <div className="clearfix">
+              {/* Content Column */}
+              <div className="content-column col-lg-8 col-md-12 col-sm-12">
+                <div className="inner-column">
+                  <div className="sec-title first">
+                    <h2 className="pushdown25">
+                      Looking for Dallas Home Watch?
+                    </h2>
+                  </div>
+                  <div className="text">
+                    <p>
+                      Dallas Home Watch is listed on many directories online but
+                      often we are not able to select the proper category of
+                      "Home Watch" because it is not known. So we are often
+                      asked to select something that closely resembles the
+                      service we provide. Listed below are some of the places
+                      you will find Dallas Home Watch listed in a directory but
+                      found in a category other than "Home Watch".
+                    </p>
+                    <Link
+                      rel="nofollow"
+                      href="https://foursquare.com/v/dallas-home-watch/63f8027b1dc7c94f6cd87baf"
+                      target="_blank"
+                    >
+                      FourSquare - Sitting Services, Home Inspection
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.superpages.com/dallas-tx/bpp/dallas-home-watch-569803047"
+                      target="_blank"
+                    >
+                      SuperPages&reg; - Sitting Services, Home Inspection
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.showmelocal.com/35812589-dallas-home-watch-farmers-branch"
+                      target="_blank"
+                    >
+                      Show Me Local - House Sitters
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.elocal.com/profile/dallas-home-watch-21675277"
+                      target="_blank"
+                    >
+                      eLocal - Home Inspections
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://citysquares.com/b/dallas-home-watch-25160800"
+                      target="_blank"
+                    >
+                      City Squares - Inspection Services
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.brownbook.net/business/51469181/dallas-home-watch"
+                      target="_blank"
+                    >
+                      Brown Book - Business
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.cylex.us.com/company/dallas-home-watch-36814039.html"
+                      target="_blank"
+                    >
+                      Cylex - Real Estate Services - Home Watch Services
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://tx.yalwa.com/ID_140825871/Dallas-Home-Watch.html?srl=1680219178"
+                      target="_blank"
+                    >
+                      Yalwa - Home Improvement Texas
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://biz.yelp.com/biz_info/XGhsW7WgIYkRMw1plCdJFw"
+                      target="_blank"
+                    >
+                      Yelp - Home Services - Home Inspections
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.hotfrog.com/company/b34d0b41017187a8ab25a14d3edd93fc"
+                      target="_blank"
+                    >
+                      Hotfrog - Home Security & Automation{" "}
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.pages24.com/dallas-tx/17620051-dallas-home-watch"
+                      target="_blank"
+                    >
+                      {" "}
+                      Pages24 - Home Watch
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.golocal247.com/biz/dallas-home-watch/dallas-tx/1313571"
+                      target="_blank"
+                    >
+                      Go Local - Home Inspections
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://www.merchantcircle.com/dallas-home-watch-dallas-tx"
+                      target="_blank"
+                    >
+                      Merchant Circle - Home Inspections
+                    </Link>
+                    <br />
+                    <Link
+                      rel="nofollow"
+                      href="https://ezlocal.com/tx/dallas/home-business/0917634495"
+                      target="_blank"
+                    >
+                      ezLocal- Home Business
+                    </Link>
+                    <br /> <br />
+                    <div className="image">
+                      <Image
+                        src="/images/logos/DWH-onWhite.jpg"
+                        alt="Dallas Home Watch logo"
+                        width={200}
+                        height={100}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Image Column */}
+              <div className="image-column col-lg-4 col-md-12 col-sm-12">
+                <div className="inner-column">
+                  <div
+                    className="image wow fadeInLeft pushdown50"
+                    data-wow-delay="0ms"
+                    data-wow-duration="1500ms"
+                  >
+                    <div className="image centered">
+                      <Image
+                        src="/images/tina-lawson-400x400.jpg"
+                        alt="Tina Lawson"
+                        width={400}
+                        height={400}
+                      />
+                      <br />
+                      Photo of{" "}
+                      <Link href="https://www.linkedin.com/in/tina-lawson-a57252254/">
+                        Tina Lawson
+                      </Link>
+                      <br />
+                      Owner of&nbsp;
+                      <Link href="https://www.linkedin.com/company/dallas-home-watch">
+                        Dallas Home Watch
+                      </Link>
+                      <br />
+                    </div>
+                  </div>
+
+                  <p>
+                    <br />
+                    <br />
+                    Dallas Home Watch LLC
+                    <br />
+                    13101 Preston Road Suite 110410
+                    <br />
+                    Dallas, Texas 75240
+                    <br />
+                    972-982-7050
+                    <br />
+                  </p>
+                  <br />
+                  <h4>Dallas Home Watch on Facebook</h4>
+                  <Link href="https://www.facebook.com/dallashomewatch">
+                    Like & Follow
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default WhereToFindPage;
